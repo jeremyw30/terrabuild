b6e76a4e75162c84a9bb5cd021daa57ada5e6d16
Jeremy <jeremy.wengler@live.be>
2025-09-17 08:48:36 +0200

    connexion/inscription >OK

---
+++ src/Controller/EntrepriseController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Contrôleur de la page Mon entreprise.
+ */
+class EntrepriseController extends AbstractController
+{
+    /**
+     * Page Mon entreprise
+     * @return Response
+     */
+    #[Route('/entreprise', name: 'app_entreprise')]
+    public function index(): Response
+    {
+        return $this->render('entreprise/index.html.twig');
+    }
+}
