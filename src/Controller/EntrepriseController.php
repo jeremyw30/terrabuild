43adb7e1609ec6d60ccecf6a1c18e73fabc54aa4
Jeremy <jeremy.wengler@live.be>
2025-09-18 07:46:56 +0200

    barre nav >à  modifié

--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -18,7 +18,37 @@ class EntrepriseController extends AbstractController
     #[Route('/entreprise', name: 'app_entreprise')]
     public function index(): Response
     {
-    $this->addFlash('success', 'Connexion réussie. Bienvenue sur votre espace entreprise TerraBuild !');
-    return $this->render('entreprise/index.html.twig');
+        $this->addFlash('success', 'Connexion réussie. Bienvenue sur votre espace entreprise TerraBuild !');
+        return $this->render('entreprise/index.html.twig');
+    }
+
+    #[Route('/entreprise/infos', name: 'app_entreprise_infos')]
+    public function infos(): Response
+    {
+        return $this->render('entreprise/infos.html.twig');
+    }
+
+    #[Route('/entreprise/materiels', name: 'app_entreprise_materiels')]
+    public function materiels(): Response
+    {
+        return $this->render('entreprise/materiels.html.twig');
+    }
+
+    #[Route('/entreprise/batiments', name: 'app_entreprise_batiments')]
+    public function batiments(): Response
+    {
+        return $this->render('entreprise/batiments.html.twig');
+    }
+
+    #[Route('/entreprise/employes', name: 'app_entreprise_employes')]
+    public function employes(): Response
+    {
+        return $this->render('entreprise/employes.html.twig');
+    }
+
+    #[Route('/entreprise/contrats', name: 'app_entreprise_contrats')]
+    public function contrats(): Response
+    {
+        return $this->render('entreprise/contrats.html.twig');
     }
 }
